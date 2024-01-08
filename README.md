562dd681322ff0c5f60e6ef912031e74a2d6c714
magicary <behnke.ca@gmail.com>
2024-01-07 23:10:37 -0600

    updated java and readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JavaSPasswordGen
 challenge 3 JavaScript password generator 
+
+This application is an easy way to create unique randomized passwords that match whatever criteria that you wish to follow. When you hit the generate button the application will ask you a series of questions in prompts including length of password and what types of characters to include in the password. 
+
+This challenge was very tricky for me as during my classes i felt i understood the material but when it came to application i struggled. Once I realixed what i was missing in the application it worked out well but it was a bit of a struggle to figure it out.
---
+++ script.js
@@ -0,0 +1,54 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
+// Write password to the #password input
+function writePassword() {
+  
+  var char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowerchar = "abcdefghijklmnopqrstuvwxyz"
+  var numbers = "0123456789"
+  var symbols = "!@#$%^&*?"
+  var passwordlength = prompt("How long would you like your password?")
+  var password ="";
+  if (passwordlength < 8 || passwordlength > 128 || Number.isNaN(passwordlength)){
+    alert("Must choose a number greater than 8 or less than 128.")
+    return null
+  }
+  var wantchar = confirm("Would you like uppercase characters?")
+  if (wantchar === true) {
+    password += char
+  }
+  var wantlowerchar = confirm("Would you like lowercase characters?")
+  if (wantlowerchar === true){
+    password += lowerchar
+  }
+  var wantnumbers = confirm("Would you like numbers?")
+  if (wantnumbers === true){
+    password += numbers
+  }
+  var wantsymbols = confirm("Would you like symbols?")
+  if (wantsymbols === true){
+    password += symbols
+  }
+  if (!wantchar && !wantlowerchar && !wantnumbers && !wantsymbols){
+    alert("Must choose atleast one option")
+    return null
+  }
+
+console.log(password)
+
+var finalpassword = ""
+  for (var i = 0; i < passwordlength; i++){
+  var randompswd = Math.floor(Math.random() * password.length);
+finalpassword += password[randompswd]
+  }
+  passwordText.value = finalpassword;
+
+}
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
